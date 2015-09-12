89df6835968e83779b3741f8455ffb4baa7aa018
Eneko <contact@eneko.me>
2015-09-12 21:39:11 +0200

    Indentation and style changes.

--- index.js
+++ index.js
@@ -1,43 +1,41 @@
-var
-    Hogan = require('hogan.js'),
+var Hogan = require('hogan.js'),
     path = require('path'),
     through = require('through2');
 
 var defaultExtensions = ['.hogan', '.mustache', '.ms'];
 
 module.exports = function hoganify (file, options) {
-    var extensions = defaultExtensions.concat(options.ext &&
-                                              options.ext.split(',') || []);
+  var extensions = defaultExtensions.concat(options.ext &&
+                        options.ext.split(',') || []);
 
-    if (extensions.indexOf(path.extname(file)) === -1) {
-        return through();
+  if (extensions.indexOf(path.extname(file)) === -1) {
+    return through();
+  }
+
+  var buffer = '';
+
+  function write (chunk, enc, next) {
+    buffer += chunk;
+    next();
+  }
+
+  function end () {
+    var template = null,
+        text = JSON.stringify(buffer),
+        compiled = '';
+
+    compiled += "var Hogan = require('hogan.js');\n";
+    if (options.live) {
+      compiled += "module.exports = Hogan.compile(" + text + ");";
+    }
+    else {
+      template = Hogan.compile(buffer, { asString: true });
+      compiled += "module.exports = new Hogan.Template(" + template + ", " + text + ", Hogan);";
     }
 
-    var buffer = '';
+    this.push(compiled);
+    this.push(null);
+  }
 
-    function write (chunk, enc, next) {
-        buffer += chunk;
-        next();
-    }
-
-    function end () {
-        var
-            template = null,
-            text = JSON.stringify(buffer),
-            compiled = '';
-
-        compiled += "var Hogan = require('hogan.js');\n";
-        if (options.live) {
-            compiled += "module.exports = Hogan.compile(" + text + ");";
-        }
-        else {
-            template = Hogan.compile(buffer, { asString: true });
-            compiled += "module.exports = new Hogan.Template(" + template + ", " + text + ", Hogan);";
-        }
-
-        this.push(compiled);
-        this.push(null);
-    }
-
-    return through(write, end);
+  return through(write, end);
 };
